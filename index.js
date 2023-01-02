40bc8dca5a3094eb1ada0684073d20a4ef8ca6ee
t3stSkill <103128976+t3stSkill@users.noreply.github.com>
2023-01-02 10:08:48 +0000

    fn

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 var readlineSync = require('readline-sync')
 
-var count = 0;
-var userRes = readlineSync.question('Enter your Age: ')
-
-if (userRes === 'yes'|| userRes === 'y') {
-  console.log('Fuck Hard')
-  // console.log(count+=1)
-  console.log(++count)
-} else {
-  console.log('Try again leter!!!')
+function addTwoNumber(paramOne, paramTwo) {
+  var sum = paramOne + paramTwo
+  return sum
 }
+
+
+
+var numOne = readlineSync.question('Enter the first value: ')
+var numTwo = readlineSync.question('Enter the second value: ')
+
+console.log('Variable call ' + addTwoNumber(Number(numOne), Number(numTwo)))
+console.log('Direct call ' + addTwoNumber(20, 7))
+
